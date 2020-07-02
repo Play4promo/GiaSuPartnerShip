ff6458e64794d3f3e7ae1f1ce3dc4cf97e944fdd
Võ Vũ Thiếu Quân <quanvo@MacBook-Pro-cua-Vo.local>
2020-07-02 19:50:22 +0700

    Version 1.0.0

---
+++ StuHubPartnerShip/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StuHubPartnerShip
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void SignIn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new DashBoard());
+        }
+    }
+}
